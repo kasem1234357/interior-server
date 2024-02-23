491a3f2840714823cedc5ec302cea97f872ef70d
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2024-02-23 10:12:34 +0300

    Update useFetsh.jsx

--- client/src/hooks/useFetsh.jsx
+++ client/src/hooks/useFetsh.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 const fetch_data =(method,subUrl,data)=>{
- return axios[method](`http://localhost:8800/${subUrl}`,data)
+ return axios[method](`https://interior-server-xxlm.onrender.com/${subUrl}`,data)
 }
 export const useFetsh = (method,subUrl,action,data=null)=>{
  const onSuccess =(data =>{
